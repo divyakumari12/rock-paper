218a35430bc8a8cdcff5b8c6feac4da2759f150a
Divya7364 <divya7364sharma@gmail.com>
2023-04-15 19:57:27 +0530

    1st change

---
+++ Desktop/React tutorial/hello-world/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './Components/greet';
+import Greet from './Components/greet';
+import Hello from './Components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Greet></Greet> */}
+
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/React tutorial/hello-world/src/Components/greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// function Greet(){
+//     return <h1>Hello Divya</h1>
+// }
+
+const Greet=()=><h1>Hello Divya</h1> // using export here will take the exact component name in the import in App.js
+
+export default Greet
